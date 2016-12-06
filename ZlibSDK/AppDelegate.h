58cb26e6ddcf7c26e6d82ceb0a1cd9d13f846760
jhcknde <jhcknde@163.com>
2016-12-06 14:37:08 +0800

    init

---
+++ ZlibSDK/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZlibSDK
+//
+//  Created by zt on 2016/12/6.
+//  Copyright © 2016年 zt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZlibSDK/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZlibSDK
+//
+//  Created by zt on 2016/12/6.
+//  Copyright © 2016年 zt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
